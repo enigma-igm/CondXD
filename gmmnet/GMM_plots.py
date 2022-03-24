48524a5b49e0ed1c8374020185cea8eef785f6fb
Rik-ad-astra <riccardo.nanni@hotmail.it>
2022-03-24 14:46:03 +0100

    Added class that compute probabilities

--- gmmnet/GMM_plots.py
+++ gmmnet/GMM_plots.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 
 import os
 import numpy as np
@@ -50,3 +51,73 @@ def make_gif(name, save_name):
     files = [folder+f'{_}_relative_flux.png' for _ in name]
     images = [imageio.imread(file) for file in files]
     imageio.mimwrite(folder+save_name+'.gif', images, fps=1.5)
+
+def plot_mag_distribution_data(bin_mp,hist,area,popt,xc,spl,limits,extension_name='data'):
+    """ Plot the mag distribution
+
+        Args:
+            bin_mp (narray): the bin mid points of the distribution
+            hist (narray): the number counts
+            area (float): the area of the survey
+            popt (list, shape nx2): the parameters of the power-law that accounts for the incompleteness
+            xc (float): the incompleteness magnitude value
+            spl (spline function): the spline used to interpolate the distribution
+            limits (list, shape nx2): the ranges to plot
+            extension_name (string): the specific name of the plot
+        """
+    create_fig_folder()
+
+    def plaw(x, popt, spl, xc):
+        if (x > xc):
+            return np.power(10, popt[0]) * x ** popt[1]
+        else:
+            return spl(x)
+
+    fig = plt.figure(num=None, figsize=(15, 10))
+    ax = plt.subplot(1, 1, 1)
+    ax.set_yscale('log')
+    ax.tick_params(axis='both', which='minor', direction='in', length=5, labelsize=30, width=4)
+    ax.tick_params(axis='both', which='major', direction='in', length=10, labelsize=30, width=4)
+    ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    ax.set_ylabel("N [deg$^{-2}$]", fontsize=35)
+    ax.set_xlabel("$J$ [mag]", fontsize=35)
+    ax.errorbar(bin_mp, hist/area,yerr=np.sqrt(hist)/area, c='k',marker='o', markersize=10,lw=0)
+    x_inter = np.linspace(min(bin_mp), max(bin_mp), 100)
+    powerl = np.array([plaw(x_inter[i], popt, spl, xc) for i in range(len(x_inter))])
+    spline= np.array([spl(x_inter[i]) for i in range(len(x_inter))])
+    ax.plot(x_inter, powerl, 'r-', markersize=5, linewidth=4, label=extension_name)
+    ax.set_xlim(limits)
+    ax.set_ylim([1e-1, 5e3])
+    ax.legend(loc='upper left', fontsize='35')
+    plt.tight_layout()
+    fig.savefig('figs/'+extension_name+'_mag_distribution.pdf')
+    plt.close()
+
+def plot_prob_hist(prob, bins=100, label='', extension_name='data'):
+    """ Plot the total probability distribution
+
+        Args:
+            prob (narray): the total probability
+            bins (int): the number of bins to use in the histogram
+            extension_name (string): the label to show in the histogram
+            extension_name (string): the specific name of the plot
+        """
+    fig=plt.figure(num=None, figsize=(15, 10))
+    ax = plt.subplot(1, 1, 1)
+    ax.tick_params(axis='both', which='minor', direction='in', length=5, labelsize=30, width=4)
+    ax.tick_params(axis='both', which='major', direction='in', length=10, labelsize=30, width=4)
+    ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    ax.yaxis.set_minor_locator(AutoMinorLocator(10))
+    ax.set_ylabel("N", fontsize=35)
+    ax.set_xlabel("P", fontsize=35)
+    ax.hist(prob, bins=bins, range=[0,1],color='k', histtype='step', lw=3,label=label)
+    ax.set_xlim([-0.1,1.1])
+    ax.set_yscale('log')
+    ax.legend(loc='upper right', fontsize='35')
+    plt.tight_layout()
+    fig.savefig('figures/'+extension_name+'_prob_hist.pdf')
+    plt.close()
---
+++ gmmnet/GMM_probabilities.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+import os
+
+import numpy as np
+
+from scipy.optimize import curve_fit
+from scipy import interpolate
+
+from astropy.table import Table
+
+import torch
+
+from gmmnet.GMM_training import GMM
+from gmmnet.GMM_plots import plot_mag_distribution_data
+
+from models import model
+
+from IPython import embed
+
+def _create_prob_folder():
+    # Chek the existence of the probabilities folders and creates it in case it is missing, so to store the plots
+    path = os.getcwd()
+    if os.path.isdir('probabilities'):
+        print("Directory probabilities already exists")
+    else:
+        print("Creating the directory: probabilities")
+        os.mkdir(path + '/probabilities')
+
+def interpolate_number_counts(ref_mag, area, bins=10, range=[17, 23], x_incom=[20, 20.5], name='data'):
+    """ Fit the number count distribution using a broken power-law
+
+        Args:
+            ref_mag (nparray): this is the mag array
+            area: area of the survey used to normalize
+            bins (int): number of bins to compute the number counts
+            range (list, shape nx2): the interpolation range
+            x_incom (list, shape nx2): the range from where to derive the incompleteness
+            name (string): the name of the plot of the distribution
+        """
+    hist, bin_edges = np.histogram(ref_mag, bins=bins,range=range)
+    bin_mp = (bin_edges[1:] + bin_edges[:-1]) / 2
+
+    # Define the power-law function that extrapolate the incompleteness
+    def pl(x, b, a):
+        y = []
+        for xx in x:
+            if (xx > np.log10(x_incom[0])):
+                y.append(b + xx * a)
+            else:
+                y.append(0)
+        return y
+
+    hist_pl = hist[(bin_mp <= x_incom[1])] / area
+    bin_mp_pl = bin_mp[(bin_mp <= x_incom[1])]
+    log_bin_mp = np.log10(bin_mp_pl)
+    log_hist = np.log10(hist_pl)
+    hist_err = np.log(10) / np.sqrt(hist_pl * area)
+    popt, pcov = curve_fit(pl, log_bin_mp, log_hist, sigma=hist_err)
+
+    # Here it computes the incompleteness curve
+    spl = interpolate.interp1d(bin_mp, hist/area, kind='cubic', fill_value="extrapolate")
+
+    # Plot the distribution
+    plot_mag_distribution_data(bin_mp, hist, area, popt, x_incom[0], spl, range, extension_name=name)
+
+    prior_prob = np.array([_prior(mag, popt, spl, x_incom[0]) for mag in ref_mag])
+
+    return prior_prob
+
+def log_prob_computation(GMM_params, hyper_params, model_name, table_name, overwrite=True):
+    """ Compute the probability density of the provided sample wrt the specified model
+
+        :param GMM_params:
+        :param hyper_params:
+        :param table_name:
+        :param overwrite:
+        """
+
+    _create_prob_folder()
+
+    # Initialize the NN and sample the noisy and noiseless data
+    XD = GMM(GMM_params, hyper_params)
+
+    best_model = model.GMMNet(XD.n_gauss, XD.rel_flux.shape[1], conditional_dim=1)
+    best_model.load_state_dict(torch.load('XD_fit_models/{}.pkl'.format(model_name)))
+    best_model.eval()
+    log_prob_dens = best_model.log_prob_b(XD.rel_flux, XD.ref_f, noise=XD.rel_flux_err).detach().numpy()
+
+    # Save the computed probabilities
+    idx = np.linspace(1, len(log_prob_dens), len(log_prob_dens))
+    catalog = np.vstack((idx, np.transpose(log_prob_dens)))
+    dat = Table(np.transpose(catalog), names=['idx', 'logP'])
+    dat.write('probabilities/{}_probabilities.fits'.format(table_name), format='fits', overwrite=overwrite)
+
+    return log_prob_dens
+
+def _prior(x, popt, spl, xc):
+    """ Compute the priors of the distribution
+
+    :param x:
+    :param popt:
+    :param spl:
+    :param xc:
+    """
+    if (x > xc):
+        return np.power(10, popt[0]) * x ** popt[1]
+    else:
+        return spl(x)
