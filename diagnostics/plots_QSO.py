96b0399b6010d809445cdf5720c365e4d8624681
Protesticon <kangyi@pku.edu.cn>
2025-06-28 16:02:28 -0700

    cleaning

--- diagnostics/plots_QSO.py
+++ diagnostics/plots_QSO.py
@@ -7,9 +7,6 @@ import torch
 import corner
 import copy
 
-
-from condxd.model import mvn
-
 def cornerplots(data_tes, output_tes, labels, bins, ranges, anno, name, noisy=False):
     """Making comparison corner plots of the test set.
 
@@ -23,28 +20,31 @@ def cornerplots(data_tes, output_tes, labels, bins, ranges, anno, name, noisy=Fa
         name (string): the specific name of the plot of relative fluxes.
         noisy (bool, optional): if the output data are samples from NN with noise. Defaults to False.
     """
-    fig = corner.corner(data_tes, labels=labels, label_kwargs={"fontsize": 25}, bins=bins, range=ranges)
+    
+    fig = corner.corner(data_tes, labels=labels, label_kwargs={"fontsize": 25}, bins=bins, range=ranges,
+                        figsize=(20,20))
     corner.corner(output_tes, fig=fig, color='r', labels=labels, label_kwargs={"fontsize": 25}, bins=bins, range=ranges, alpha=0.7)
     axes = np.array(fig.axes).reshape((data_tes.shape[-1], data_tes.shape[-1]))
     for ax in fig.get_axes():
         ax.tick_params(axis='both', which='major', direction='in', length=5, labelsize=15, width=2)
         ax.xaxis.set_ticks_position('both')
         ax.yaxis.set_ticks_position('both')
-    axes[0, -3].text(0.6, 0.4, anno, c='k',
+    axes[0, -3].text(0.7, 0.4, anno, c='k',
                 fontsize=20, horizontalalignment='center', weight='bold')
-    axes[1, -3].text(0.6, 1.0, f'Test Set', c='k',
+    axes[1, -3].text(0.7, 1.0, f'Test Set', c='k',
                 fontsize=20, horizontalalignment='center', weight='bold')
     if noisy is False:
-        tag = 'Deconvolved'
+        tag = 'Noise Deconvolved'
     if noisy is True:
-        tag = 'Reconvolved'
-    axes[1, -3].text(0.6, 0.8, f'Samples on Conditional XD ({tag})', c='r',
+        tag = 'Noise Reconvolved'
+    axes[1, -3].text(0.65, 0.8, f'Samples on CondXD ({tag})', c='r',
                 fontsize=20, horizontalalignment='center', weight='bold')
     if noisy is False:
         tag = 'clean'
     if noisy is True:
         tag = 'noisy'
-    fig.savefig(f'figs/{tag}_relative_flux_'+name+'.png')
+    fig.savefig(f'figs/{tag}_relative_flux_'+name+'.pdf')
+    plt.close()
     
 
 
@@ -55,7 +55,7 @@ def make_gif(Jbin_len,K,tag,save_name):
     files = [folder+f'{tag}_relative_flux_d_J{i:d}_K{K:d}.png'
                  for i in range(Jbin_len)]
     images = [imageio.imread(file) for file in files]
-    imageio.mimwrite(folder+save_name+'.gif', images, fps=1.5)
+    imageio.mimwrite(folder+save_name+'.gif', images, duration=670)
 
 def f2mag(f):
     return 22.5 - 2.5 * np.log10(f)
@@ -65,11 +65,14 @@ def mag2f(mag):
 
 
 
-def all_figures(K,
-                D,
-                test_loader,
-                gmm
-                ):
+def all_figures(
+        K,
+        D,
+        test_loader,
+        gmm,
+        data_avg,
+        data_std
+    ):
     """All figures to show the performances of our network.
 
     Args:
@@ -80,8 +83,9 @@ def all_figures(K,
     """
 
     # parameters for plotting
-    bins=25
-    labels = ['$f_z$','$f_Y$', '$f_H$','$f_{Ks}$','$f_{W1}$','$f_{W2}$']
+    bins = 25
+    labels = ['$f_z/f_J$','$f_Y/f_J$', '$f_H/f_J$','$f_{Ks}/f_J$',
+              '$f_{W1}/f_J$','$f_{W2}/f_J$']
     ranges = [(-0.5,1.),(-0.7,1.4),(-1.2,3.1),(-1.6,5.),(-2.5,8.3),(-4,12)]
     # parameters of plots in J band flux bins
     path = 'data/'
@@ -97,15 +101,15 @@ def all_figures(K,
     #All figures to show the performances of our network.
 
     # sampling from trained model with noise
-    f_J_tes = torch.Tensor([])
+    mag_J_tes = torch.Tensor([])
     data_tes = torch.Tensor([]) 
     output_tes = torch.Tensor([])
-    for i, (f_J_i, data_i, err_r_i) in enumerate(test_loader):
-        f_J_tes = torch.cat((f_J_tes, f_J_i))
+    for i, (mag_J_i, data_i, err_r_i) in enumerate(test_loader):
+        mag_J_tes = torch.cat((mag_J_tes, mag_J_i))
         data_tes = torch.cat((data_tes, data_i))
-        output_tes = torch.cat((output_tes, gmm.sample(f_J_i, 1, err_r_i)))
-    data_tes = data_tes.numpy()
-    output_tes = output_tes.reshape(-1, D).numpy()
+        output_tes = torch.cat((output_tes, gmm.sample(mag_J_i, 1, err_r_i)))
+    data_tes = data_tes.numpy() * data_std.numpy() + data_avg.numpy()
+    output_tes = output_tes.reshape(-1, D).numpy() * data_std.numpy() + data_avg.numpy()
         
     # the corner plot of the relative fluxes
     name = f'd_K{K:d}'
@@ -113,13 +117,14 @@ def all_figures(K,
     
     # the corner plot of the relative fluxes in each J band flux bin
     for i in range(Jbin_len):
-        bln = (torch.exp(f_J_tes)<mag2f(Jbin_l[i])) & (torch.exp(f_J_tes)>=mag2f(Jbin_r[i]))
+        bln = (mag_J_tes>Jbin_l[i]) & (mag_J_tes<=Jbin_r[i])
         bln = bln.numpy().flatten()
         data_tes_i = data_tes[bln]
         output_tes_i = output_tes[bln]
         anno = f'{Jbin_l[i]:.1f}<$J$<{Jbin_r[i]:.1f}'
         name = f'd_J{i:d}_K{K:d}'
         cornerplots(data_tes_i, output_tes_i, labels, bins, ranges, anno, name, noisy=True)
+
 
     tag='noisy'
     save_name = f'noisy_relative_f_d_K{K:d}'
@@ -129,16 +134,16 @@ def all_figures(K,
 
 
     # shuffle the test set
-    f_J_tes = torch.Tensor([])
+    mag_J_tes = torch.Tensor([])
     data_tes = torch.Tensor([])
     # sampling from trained model without noise
     output_tes = torch.Tensor([])
-    for i, (f_J_i, data_i, _) in enumerate(test_loader):
-        f_J_tes = torch.cat((f_J_tes, f_J_i))
+    for i, (mag_J_i, data_i, _) in enumerate(test_loader):
+        mag_J_tes = torch.cat((mag_J_tes, mag_J_i))
         data_tes = torch.cat((data_tes, data_i))
-        output_tes = torch.cat((output_tes, gmm.sample(f_J_i, 1)))
-    data_tes = data_tes.numpy()
-    output_tes = output_tes.reshape(-1, D).numpy()
+        output_tes = torch.cat((output_tes, gmm.sample(mag_J_i, 1)))
+    data_tes = data_tes.numpy() * data_std.numpy() + data_avg.numpy()
+    output_tes = output_tes.reshape(-1, D).numpy() * data_std.numpy() + data_avg.numpy()
 
     # the corner plot of the relative fluxes
     name = f'd_K{K:d}'
@@ -146,7 +151,7 @@ def all_figures(K,
     
     # the corner plot of the relative fluxes in each J band flux bin
     for i in range(Jbin_len):
-        bln = (torch.exp(f_J_tes)<mag2f(Jbin_l[i])) & (torch.exp(f_J_tes)>=mag2f(Jbin_r[i]))
+        bln = (mag_J_tes>Jbin_l[i]) & (mag_J_tes<=Jbin_r[i])
         bln = bln.numpy().flatten()
         data_tes_i = data_tes[bln]
         output_tes_i = output_tes[bln]
